b5ecd68015c2b0b620986b11241a83ef093f847d
Nishith Savla <nishithsavla005@gmail.com>
2022-10-02 17:28:25 +0530

    Refactor code to implement best practices and improve readability
    - Also update the sample HN item
    - Replace multiple searches with a single search in applyStyles

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 /*
 sample HN item:
 
-<tr class="athing" id="32987028">
-	<td align="right" valign="top" class="title"><span class="rank">27.</span></td>
-	<td valign="top" class="votelinks">
-		<center>
-			<a id="up_32987028" class="clicky" href="vote?id=32987028&amp;how=up&amp;auth=f127c4b9607ee6f58efc7de0790ff40256f36bc8&amp;goto=news">
-				<div class="votearrow" title="upvote"></div>
-			</a>
-		</center>
-	</td>
-	<td class="title"><a href="https://vivqu.com/blog/2022/09/25/outdated-apps/" class="titlelink" style="font-size: 30px;">Outdated vs. Complete: In defense of apps that don’t need updates</a><span class="sitebit comhead"> (<a href="from?site=vivqu.com"><span class="sitestr">vivqu.com</span></a>)</span></td>
+<tr class="athing" id="33054379">
+    <td align="right" valign="top" class="title"><span class="rank">1.</span></td>
+    <td valign="top" class="votelinks">
+        <center>
+            <a id="up_33054379" href="vote?id=33054379&amp;how=up&amp;goto=news">
+                <div class="votearrow" title="upvote"></div>
+            </a>
+        </center>
+    </td>
+    <td class="title"><span class="titleline" style="font-size: 11px;"><a href="https://gist.github.com/susam/75c37fd0aff9c5e25112eac75b9ed055">From 7 Years of Apache HTTP Server Logs: 5528 Unique Recon and Attack Vectors</a><span class="sitebit comhead"> (<a href="from?site=gist.github.com"><span class="sitestr">gist.github.com</span></a>)</span></span></td>
 </tr>
 */
 
-var config = {
+const config = {
     minFont: 10, //in pixels
     maxFont: 30, //in pixels
-    defaultFont:13.33, //in pixels    
+    defaultFont: 13.33, //in pixels
     pointsWeight: 0.5, // 0-1, must complement commentsWeight
-    commentsWeight: 0.5 // 0-1, must complement commentsWeight
-}
+    commentsWeight: 0.5, // 0-1, must complement pointWeight
+};
 
 /*
-the "map" object is a global data structure in the format of:
-[
-    {
-        id: "12748863",
-        upvote: 1412,
-        size: 30
-    }
-]
+the "newsItems" array is a global array containing news item objects in the format of:
+{
+    id: "12748863",
+    upvote: 1412,
+    size: 30
+}
 */
-var map = [];
+const newsItems = [];
 
 buildIdPointsCommentsMap();
 calculatePriority("points", "pointsRank");
@@ -41,59 +39,53 @@ calculateFinalSize();
 applyStyle();
 
 
-
 function buildIdPointsCommentsMap() {
-    var subtextElementsTd = document.getElementsByClassName("subtext");
-    Array.prototype.forEach.call(subtextElementsTd, function (subtextElementTd) {
-        var scoreElementSpan = subtextElementTd.firstElementChild.firstElementChild;
+    const subtextElementsTd = document.getElementsByClassName("subtext");
+    for (let subtextElementTd of subtextElementsTd) {
+        const scoreElementSpan = subtextElementTd.firstElementChild?.firstElementChild;
         //sample match: <span class="score" id="score_12748863">1412 points</span>
-        if (scoreElementSpan.className !== "score") { return; } //some items cant be voted, exclude them
-        var id = scoreElementSpan.id.replace("score_", "");
-        var points = parseInt(scoreElementSpan.innerText.replace(" points", ""));
-        var commentsElementA = subtextElementTd.lastElementChild;
-        //sample match: <a href="item?id=12748863">1069&nbsp;comments</a>
-        //or: <a href="item?id=12748863">discuss</a>
-        if ((scoreElementSpan.innerText.indexOf("comments") != -1) || (scoreElementSpan.innerText.indexOf("discuss") != -1 )) { return; } //some items cant be discussed (?), exclude them
-        var comments = parseInt(commentsElementA.innerText.replace(" comments", ""));
-        map.push({ id: id, points: points, comments: comments, size: config.defaultFont });
-    });
+        if (scoreElementSpan?.className !== "score") continue; //some newsItems cant be voted, exclude them
+
+        //sample match: <a href="item?id=12748863">1069&nbsp;comments</a> or <a href="item?id=12748863">discuss</a>
+        if (scoreElementSpan.innerText.includes("comments") || scoreElementSpan.innerText.includes("discuss")) continue; //some newsItems cant be discussed (?), exclude them
+
+        const id = scoreElementSpan.id.replace("score_", "");
+        const points = parseInt(scoreElementSpan.innerText); // no need to remove " points" as `parseInt` already handles that intelligently
+        const comments = parseInt(subtextElementTd.lastElementChild.innerText); // no need to remove " comments" as `parseInt` already handles that intelligently
+
+        newsItems.push({ id, points, comments, size: config.defaultFont });
+    }
 }
 
 function calculatePriority(inputName, outputName) {
-    var min = Number.MAX_VALUE;
-    var max = 0;
+    let min = Number.MAX_VALUE;
+    let max = 0;
 
-    for (var i = 0; i < map.length; i++) {
-        var val = map[i][inputName];
-        if (val > max) {
-            max = val;
-        }
-        if (val < min) {
-            min = val;
-        }
-    }
+    newsItems.forEach(item => {
+        const val = item[inputName];
+        if (val > max) max = val;
+        if (val < min) min = val;
+    });
 
-    for (var i = 0; i < map.length; i++) {
-        var val = map[i][inputName];
-        var size = ((config.maxFont - config.minFont) * (val - min) / (max - min)) + config.minFont;
-        size = Math.round(size);
-        map[i][outputName] = size;
-    }
+    newsItems.forEach(item => {
+        const val = item[inputName];
+        const size = ((config.maxFont - config.minFont) * (val - min) / (max - min)) + config.minFont;
+        item[outputName] = Math.round(size);
+    });
 }
 
 function calculateFinalSize() {
-    for (var i = 0; i < map.length; i++) { //TODO: too many loops over map. Combine into calculate?
-        var item = map[i];
-        item.size = ( item.pointsRank * config.pointsWeight) + (item.commentsRank * config.commentsWeight);
-    }    
+    newsItems.forEach(item => { //TODO: too many loops over newsItems. Combine into calculate?
+        item.size = (item.pointsRank * config.pointsWeight) + (item.commentsRank * config.commentsWeight);
+    });
 }
 
 function applyStyle() {
-    for (i = 0; i < map.length; i++) {
-        var itemTrElement = document.getElementById(map[i].id); // TODO: optimize by doing one search instead of map.length searches
-        var titleTdElement = itemTrElement.getElementsByClassName("titleline")[0];
-        if (titleTdElement) {
-            titleTdElement.style.fontSize = map[i].size + "px";
-        }
+    const selectors = newsItems.map(item => `[id="${item.id}"] .titleline`).join(",");
+    const titleTdElements = document.querySelectorAll(selectors);
+
+    for (let i = 0; i < titleTdElements.length; ++i) {
+        const titleTdElement = titleTdElements.item(i);
+        if (titleTdElement) titleTdElement.style.fontSize = newsItems[i].size + "px";
     }
 }
